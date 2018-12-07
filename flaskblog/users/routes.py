7a5ea88a1beb6e7e71afec5a4da045358435a798
Roy Larson <roy.larson@gmail.com>
2018-12-07 13:34:37 -0600

    testing of video 11 stuff

--- flaskblog/users/routes.py
+++ flaskblog/users/routes.py
@@ -1,5 +1,6 @@
 from flask import (render_template, url_for, flash,
                  redirect, request, Blueprint)
+from flask_login import login_user, current_user, logout_user, login_required
 from flaskblog.models import User, Post
 from flaskblog import bcrypt, db
 from flaskblog.users.forms import (RegistrationForm, LoginForm, UpdateAccountForm,
